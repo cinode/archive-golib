ad9d3ca325a15da2329b6612fd3ec9c0194d01bf
byo <byo.spoon@gmail.com>
2013-01-25 00:15:30 +0100

    Create path-based blob storage

---
+++ blobstore/fileblobstorage.go
@@ -0,0 +1,20 @@
+package blobstore
+
+// TODO: Error handling
+
+import "io"
+import "os"
+
+func NewFileBlobStorage(path string) BlobStorage {
+	os.MkdirAll(path, 0777)
+	return &fileBlobStorage{path: path}
+}
+
+type fileBlobStorage struct {
+	path string
+}
+
+func (s *fileBlobStorage) NewBlobWriter(blobId string) io.WriteCloser {
+	fl, _ := os.Create(s.path + string(os.PathSeparator) + blobId)
+	return fl
+}
