d036948cf91a5b3f39dd76736b1e36614a41e4c2
byo <byo.spoon@gmail.com>
2013-02-02 23:17:44 +0100

    Implement simple memory blob storage

---
+++ blobstore/memoryblobstorage.go
@@ -0,0 +1,51 @@
+package blobstore
+
+// TODO: Support for duplicates (let write the blob with same id as long as the content does match)
+
+import (
+	"bytes"
+)
+
+func NewMemoryBlobStorage() BlobStorage {
+	return &memoryBlobStorage{
+		blobs: make(map[string][]byte)}
+}
+
+type memoryBlobStorage struct {
+	blobs map[string][]byte
+}
+
+type memoryBlobWriter struct {
+	storage *memoryBlobStorage
+	buffer  bytes.Buffer
+	bid     string
+}
+
+func (f *memoryBlobWriter) Write(p []byte) (n int, err error) {
+	return f.buffer.Write(p)
+}
+
+func (f *memoryBlobWriter) Finalize() error {
+	previous, exists := f.storage.blobs[f.bid]
+	if exists {
+		if !bytes.Equal(previous, f.buffer.Bytes()) {
+			return ErrBIDCollision
+		}
+	} else {
+		f.storage.blobs[f.bid] = f.buffer.Bytes()
+	}
+	return nil
+}
+
+func (f *memoryBlobWriter) Cancel() error {
+	f.buffer.Reset()
+	f.storage, f.bid = nil, ""
+	return nil
+}
+
+func (s *memoryBlobStorage) NewBlobWriter(blobId string) (writer BlobWriter, err error) {
+	return &memoryBlobWriter{
+			storage: s,
+			bid:     blobId},
+		nil
+}
