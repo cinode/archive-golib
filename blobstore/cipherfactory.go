5273208e0ab2031df0b2d5ab653fbe3783f46f95
byo <byo.spoon@gmail.com>
2013-02-13 23:41:12 +0100

    Add decryptor generation code

--- blobstore/cipherfactory.go
+++ blobstore/cipherfactory.go
@@ -10,6 +10,8 @@ import (
 
 var (
 	ErrInsufficientKeySource = errors.New("Not enough data to create a proper encryption key")
+	ErrInvalidKey            = errors.New("Invalid key")
+	ErrUnknownKeyType        = errors.New("Unknown key type")
 )
 
 func createEncryptor(keySource, ivSource []byte, output io.Writer) (writer io.Writer, key string, err error) {
@@ -46,4 +48,48 @@ func createEncryptor(keySource, ivSource []byte, output io.Writer) (writer io.Wr
 		W: output}
 
 	return
+}
+
+func createDecryptor(key string, ivSource []byte, input io.Reader) (reader io.Reader, err error) {
+	keyRaw, err := hex.DecodeString(key)
+	if err != nil || len(keyRaw) < 1 {
+		return nil, ErrInvalidKey
+	}
+
+	switch keyRaw[0] {
+	case cipherAES256:
+		return createDecryptorAES256(keyRaw[1:], ivSource, input)
+	}
+
+	return nil, ErrUnknownKeyType
+}
+
+func createDecryptorAES256(key []byte, ivSource []byte, input io.Reader) (reader io.Reader, err error) {
+
+	if len(key) != 32 {
+		return nil, ErrInvalidKey
+	}
+
+	// Normalize the iv
+	var iv [16]byte
+	for i, b := range ivSource {
+		iv[i] = b
+		if i >= 15 {
+			break
+		}
+	}
+
+	// Create new base cipher
+	blobCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	// Generate the reader in CFB mode
+	return &cipher.StreamReader{
+			S: cipher.NewCFBDecrypter(
+				blobCipher,
+				iv[:]),
+			R: input},
+		nil
 }
