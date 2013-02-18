41c4a36436ae2a47538859a7deba5266ecf93d1f
byo <byo.spoon@gmail.com>
2013-02-18 23:43:15 +0100

    Implement int/string desserialization

--- blobstore/serialization.go
+++ blobstore/serialization.go
@@ -6,6 +6,14 @@ package blobstore
 
 import (
 	"bytes"
+	"errors"
+	"io"
+	"unicode/utf8"
+)
+
+var (
+	ErrDeserializeStringToLarge = errors.New("Could not deserialize string value due to invalid length")
+	ErrDeserializeStringNotUTF8 = errors.New("Could not deserialize string value - not a UTF-8 sequence")
 )
 
 func serializeInt(v int64, buff *bytes.Buffer) {
@@ -28,4 +36,47 @@ func serializeString(s string, buff *bytes.Buffer) {
 	data := []byte(s)
 	serializeInt(int64(len(data)), buff)
 	buff.Write(data)
+}
+
+func deserializeInt(r io.Reader) (v int64, err error) {
+	// TODO: Overflows
+	v, s := 0, uint(0)
+	buff := []byte{0}
+	for ; ; s += 7 {
+
+		// Get next byte
+		if _, err = r.Read(buff); err != nil {
+			return
+		}
+
+		// Fill in the data in returned value
+		v |= int64(buff[0]&0x7F) << s
+
+		if (buff[0] & 0x80) == 0 {
+			break
+		}
+	}
+	return
+}
+
+func deserializeString(r io.Reader, maxLength int64) (v string, err error) {
+	length, err := deserializeInt(r)
+	if err != nil {
+		return
+	}
+
+	if (length < 0) || (length > maxLength) {
+		return ErrDeserializeStringToLarge
+	}
+
+	buffer := make([]byte, length)
+	if _, err = io.ReadFull(r, buffer); err != nil {
+		return
+	}
+
+	if !utf8.Valid(buffer) {
+		return ErrDeserializeStringNotUTF8
+	}
+
+	return string(buffer)
 }
