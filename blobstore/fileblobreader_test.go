cb593f94c0d4655b620874a084dd296e441f1f91
byo <byo.spoon@gmail.com>
2013-02-20 22:55:36 +0100

    Make file blob test more generic

--- blobstore/fileblobreader_test.go
+++ blobstore/fileblobreader_test.go
@@ -13,19 +13,21 @@ func putBlob(storage BlobStorage, bid string, content []byte) {
 }
 
 func TestEmptyFileBlob(t *testing.T) {
+	genericFileBlobTest(t,
+		"b4f5a7bb878c0cec9cb4bd6ae8bb175a7ea59c1a048c5ab7c119990d0041cb9cfb67c2aa9e6fada8112719777b4b80ffada80205f8ebe6981c0ade97ff3df8e5",
+		"017b54b66836c1fbdd13d2441d9e1434dc62ca677fb68f5fe66a464baadecdbd00",
+		[]byte{0x01, 0xeb},
+		[]byte{})
+}
+
+func genericFileBlobTest(t *testing.T, bid, key string, blobContent, fileContent []byte) {
 
 	storage := NewMemoryBlobStorage()
 
-	content := []byte{}
-
-	putBlob(storage,
-		"b4f5a7bb878c0cec9cb4bd6ae8bb175a7ea59c1a048c5ab7c119990d0041cb9cfb67c2aa9e6fada8112719777b4b80ffada80205f8ebe6981c0ade97ff3df8e5",
-		[]byte{0x01, 0xeb})
+	putBlob(storage, bid, blobContent)
 
 	rdr := FileBlobReader{Storage: storage}
-	err := rdr.Open(
-		"b4f5a7bb878c0cec9cb4bd6ae8bb175a7ea59c1a048c5ab7c119990d0041cb9cfb67c2aa9e6fada8112719777b4b80ffada80205f8ebe6981c0ade97ff3df8e5",
-		"017b54b66836c1fbdd13d2441d9e1434dc62ca677fb68f5fe66a464baadecdbd00")
+	err := rdr.Open(bid, key)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -35,7 +37,7 @@ func TestEmptyFileBlob(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if !bytes.Equal(content, data) {
+	if !bytes.Equal(fileContent, data) {
 		t.Error("Invalid blob content")
 	}
 }
